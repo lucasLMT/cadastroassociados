609e857ffcaa092c1fae0e7274abdf01206103f9
mateusdeleon <mateusdeleon1987@gmail.com>
2015-03-08 13:04:21 -0300

    Configuração do cake no projeto cadastroassociados.

---
+++ Controller/AreasController.php
@@ -0,0 +1,141 @@
+<?php
+App::uses('AppController', 'Controller');
+/**
+ * Areas Controller
+ *
+ * @property Area $Area
+ * @property PaginatorComponent $Paginator
+ */
+class AreasController extends AppController {
+
+/**
+ * Components
+ *
+ * @var array
+ */
+	public $components = array('Paginator');
+
+/**
+ * index method
+ *
+ * @return void
+ */
+	public function index() {
+		$this->Area->recursive = 0;
+		$this->set('areas', $this->Paginator->paginate());
+	}
+
+/**
+ * view method
+ *
+ * @throws NotFoundException
+ * @param string $id
+ * @return void
+ */
+	public function view($id = null) {
+		if (!$this->Area->exists($id)) {
+			throw new NotFoundException(__('Invalid area'));
+		}
+		$options = array('conditions' => array('Area.' . $this->Area->primaryKey => $id));
+		$this->set('area', $this->Area->find('first', $options));
+	}
+
+/**
+ * add method
+ *
+ * @return void
+ */
+	public function add() {
+		if ($this->request->is('post')) {
+			$this->Area->create();
+			if ($this->Area->save($this->request->data)) {
+				$this->Session->setFlash(__('The area has been saved.'));
+				return $this->redirect(array('action' => 'index'));
+			} else {
+				$this->Session->setFlash(__('The area could not be saved. Please, try again.'));
+			}
+		}
+	}
+
+/**
+ * edit method
+ *
+ * @throws NotFoundException
+ * @param string $id
+ * @return void
+ */
+	public function edit($id = null) {
+		if (!$this->Area->exists($id)) {
+			throw new NotFoundException(__('Invalid area'));
+		}
+		if ($this->request->is(array('post', 'put'))) {
+			if ($this->Area->save($this->request->data)) {
+				$this->Session->setFlash(__('The area has been saved.'));
+				return $this->redirect(array('action' => 'index'));
+			} else {
+				$this->Session->setFlash(__('The area could not be saved. Please, try again.'));
+			}
+		} else {
+			$options = array('conditions' => array('Area.' . $this->Area->primaryKey => $id));
+			$this->request->data = $this->Area->find('first', $options);
+		}
+	}
+
+/**
+ * delete method
+ *
+ * @throws NotFoundException
+ * @param string $id
+ * @return void
+ */
+	public function delete($id = null) {
+		$this->Area->id = $id;
+		if (!$this->Area->exists()) {
+			throw new NotFoundException(__('Invalid area'));
+		}
+		$this->request->allowMethod('post', 'delete');
+		if ($this->Area->delete()) {
+			$this->Session->setFlash(__('The area has been deleted.'));
+		} else {
+			$this->Session->setFlash(__('The area could not be deleted. Please, try again.'));
+		}
+		return $this->redirect(array('action' => 'index'));
+	}
+
+/**
+ * Displays a view
+ *
+ * @return void
+ * @throws NotFoundException When the view file could not be found
+ *	or MissingViewException in debug mode.
+ */
+	public function display() {
+		$path = func_get_args();
+
+		$count = count($path);
+		if (!$count) {
+			return $this->redirect('/');
+		}
+		$page = $subpage = $title_for_layout = null;
+
+		if (!empty($path[0])) {
+			$page = $path[0];
+		}
+		if (!empty($path[1])) {
+			$subpage = $path[1];
+		}
+		if (!empty($path[$count - 1])) {
+			$title_for_layout = Inflector::humanize($path[$count - 1]);
+		}
+		$this->set(compact('page', 'subpage', 'title_for_layout'));
+
+		try {
+			$this->render(implode('/', $path));
+		} catch (MissingViewException $e) {
+			if (Configure::read('debug')) {
+				throw $e;
+			}
+			throw new NotFoundException();
+		}
+	}
+}
