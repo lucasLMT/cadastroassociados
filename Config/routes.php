611a9a33fb017f5def287fcdff04783937faa5cd
vitorfblima <vitorfblima@gmail.com>
2015-04-27 12:54:02 -0300

    Adicionando o login.

--- Config/routes.php
+++ Config/routes.php
@@ -24,7 +24,7 @@
  * its action called 'display', and we pass a param to select the view file
  * to use (in this case, /app/View/Pages/home.ctp)...
  */
-	Router::connect('/', array('controller' => 'Associados', 'action' => 'index', 'home'));
+	Router::connect('/', array('controller' => 'Users', 'action' => 'login', 'home'));
 /**
  * ...and connect the rest of 'Pages' controller's URLs.
  */
