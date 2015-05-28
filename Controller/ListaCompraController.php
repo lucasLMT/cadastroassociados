de7049f869f88afcdc2dfc6171d8bf268ac7027a
Lucas Moreira Teles <lucasmoreiralc@gmail.com>
2015-05-27 21:47:03 -0300

    Find com "between"

--- Controller/ListaCompraController.php
+++ Controller/ListaCompraController.php
@@ -26,6 +26,7 @@ class ListaCompraController extends AppController {
  * @return void
  */
 	public function lista($compras = null) {
+		debug($compras);
 		$this->ListaCompra->recursive = 0;
 		$ListaCompras = $this->Paginator->paginate();
 		$this->set(compact('ListaCompras', 'compras'));
@@ -33,30 +34,26 @@ class ListaCompraController extends AppController {
 
 	public function form($id = null) {
 
-		//$this->loadModel('ListaCompra');
 		$model = ClassRegistry::init('ListaCompra');
 
 		if ($this->request->is('post')) {
 			App::uses('String', 'Utility');
 			$data = $this->request->data;
-			$data_inicio = String::insert(':ano/:mes/:dia', array(
+			$data_inicio = String::insert(':ano-:mes-:dia', array(
 											'ano' => $data['ListaCompra']['data_inicio']['year'],
 											'mes' => $data['ListaCompra']['data_inicio']['month'],
 											'dia' => $data['ListaCompra']['data_inicio']['day']));
-			$data_fim = String::insert(':ano/:mes/:dia', array(
+			$data_fim = String::insert(':ano-:mes-:dia', array(
 											'ano' => $data['ListaCompra']['data_fim']['year'],
 											'mes' => $data['ListaCompra']['data_fim']['month'],
 											'dia' => $data['ListaCompra']['data_fim']['day']));
 
-			$options = array('conditions' => array(//'Compra.referencia' => '2015-05-24'));//$data_inicio)); 
-												   'Compra.referencia between ? and ?'=> array(
-												   	$data_inicio, $data_fim)));
+			$options = array('conditions' => array('Compra.referencia >= ' => $data_inicio,
+								                   'Compra.referencia <= ' => $data_fim));
 			$compras = $this->ListaCompra->Compra->find('all', $options);
-			debug($compras);
-			$this->redirect(array('action' => 'form'));
+			$this->redirect(array('controller' => 'ListaCompra','action' => 'lista','compras' => $compras));
 		}
-		//$options = array('conditions' => array('Compra.associado_id' => $id));
-		$associados = $this->ListaCompra->Associado->find('list');//$model->getAssociadosList();
+		$associados = $this->ListaCompra->Associado->find('list');
 		$this->set(compact('associados'));
 	}
 }
