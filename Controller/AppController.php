d658c2addbc7c1d8b967117ff88d55e2d779a7eb
Lucas Moreira Teles <lucasmoreiralc@gmail.com>
2015-07-12 17:01:29 -0300

    Correções na data.

--- Controller/AppController.php
+++ Controller/AppController.php
@@ -30,6 +30,9 @@ App::uses('Controller', 'Controller');
  * @package        app.Controller
  * @link        http://book.cakephp.org/2.0/en/controllers.html#the-app-controller
  */
+
+App::import('Core', 'l10n');
+
 class AppController extends Controller
 {
     public $components = array(
